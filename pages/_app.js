8deb058da403096c8e94a0b128ff208cc7fb5d79
psychoutmedia <psychoutbeats@example.com>
2025-04-26 17:46:36 +0100

    Render Analytics globally

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '@/styles/globals.css';
+import { Analytics } from '@vercel/analytics/react';
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
 import AOS from 'aos';
@@ -11,5 +12,10 @@ export default function MyApp({ Component, pageProps }) {
     });
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics /> {/* <-- THIS is the missing piece */}
+    </>
+  );
 }
